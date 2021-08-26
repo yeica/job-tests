678a81e2ff0d691c19cc33849ed68dc1d5b4282c
Yeyskalyn Cordero Herrera <yeyskalyn@gmail.com>
2021-08-26 08:18:29 -0400

    some display data annotations added to Jugador model

--- MVCApp/Models/Jugador.cs
+++ MVCApp/Models/Jugador.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,13 +11,19 @@ namespace MVCApp.Models
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set;  }
+
+        [Display(Name= "Fecha de Nacimiento")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime FechaNacimiento { get; set; }
         public string Pasaporte { get; set; }
         public string Direccion { get; set; }
         public string Sexo { get; set; }
+
+        [Display(Name = "Equipo")]
         public int EquipoId { get; set; }
         public Equipo  Equipo { get; set; }
 
+        [Display(Name = "Estado")]
         public int EstadoId { get; set; }
         public Estado  Estado { get; set; }
     }
