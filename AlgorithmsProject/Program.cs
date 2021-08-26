cc5cae5644a55dcf65b5b25e7073299602f702e6
Yeyskalyn Cordero Herrera <yeyskalyn@gmail.com>
2021-08-26 07:08:22 -0400

    console project created for algorithms

---
+++ AlgorithmsProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
